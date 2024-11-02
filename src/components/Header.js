fff95a9a26fab415749e1187ca78a0c60d3fe965
Yash Shrivastava <the.revived.1@gmail.com>
2024-11-03 01:06:19 +0530

    Header color update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-primary-500 text-secondary-50 p-4">
+    <header className="bg-primary-900 text-secondary-100 p-4">
       <nav className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-bold">
           <Link href="/">Yash Shrivastava</Link>
