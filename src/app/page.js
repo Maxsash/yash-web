fcfc026cf68a0381a2afd9bfa1f9303870193f27
Yash Shrivastava <the.revived.1@gmail.com>
2024-11-02 12:15:10 +0530

    CSS fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <main className="flex flex-col sm:flex-row items-center sm:items-start gap-8 sm:gap-16 max-w-4xl mx-auto">
-        <div className="text-center sm:text-left">
+    <div className="flex items-center justify-center min-h-screen">
+      <main className="flex flex-col sm:flex-row items-start gap-8 sm:gap-16 max-w-4xl mx-auto px-4">
+        <div className="text-left">
           <h1 className="text-5xl sm:text-6xl font-bold text-gray-800 mb-2">
             Hey, I am Yash!
           </h1>
