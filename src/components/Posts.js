cb04fc6f6cec00a1a9c930fb5a7af40b45052d50
Yash Shrivastava <the.revived.1@gmail.com>
2024-11-03 09:41:57 +0530

    Fix alignment

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,8 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from '@/app/blog/utils'
+import Link from "next/link";
+import { formatDate, getBlogPosts } from "@/app/blog/utils";
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+  let allBlogs = getBlogPosts();
 
   return (
     <div>
@@ -11,26 +11,26 @@ export function BlogPosts() {
           if (
             new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
           ) {
-            return -1
+            return -1;
           }
-          return 1
+          return 1;
         })
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-primary-600 w-[100px] tabular-nums">
+            <div className="w-full flex flex-row items-center space-x-2 p-2 md:p-4 border border-gray-200 rounded-lg">
+              <p className="text-primary-600 text-sm w-[70px] tabular-nums opacity-75">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-primary-900 tracking-tight">
+              <p className="text-primary-900 font-semibold tracking-tight leading-snug md:leading-normal md:text-lg">
                 {post.metadata.title}
               </p>
             </div>
           </Link>
         ))}
     </div>
-  )
+  );
 }
